b8de39dc7e6f5db52daed0ad19e4d3251184fc2f
Zanoli <zancamar9@gmail.com>
2023-10-10 20:33:02 -0300

    "Generics"

---
+++ fundamentos/generic1.dart
@@ -0,0 +1,21 @@
+main() {
+  var listaCoisas = [
+    'Banana',
+    true,
+    3,
+    4.16,
+    [1, 2, 3]
+  ];
+  List<String> frutas = ['Banana', 'Maçã', 'Abacate'];
+
+  print(listaCoisas);
+  print(frutas);
+
+  Map<String, double> salarios = {
+    'gerente': 19345.78,
+    'vendedor': 16345.80,
+    'estagiario': 600.30,
+  };
+
+  print(salarios);
+}
